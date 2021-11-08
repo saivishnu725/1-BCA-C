e586913f17051d3ad63fd6b013948aefc5c6b1d2
Sai Vishnu <saivishnu725@gmail.com>
2021-11-08 20:33:13 +0530

    Prime

---
+++ 08-prime.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(i = 2; i < n/2; i++)
+    {
+        if( n % i == 0)
+            count++;
+        
+    }
+    if(count != 0)
+        printf("%d is not a prime number.\n", n);
+    else
+        printf("%d is a prime number.\n", n);
+    return 0;
+}
