c726792f76781e913aab095b453540417cda32ea
Sai Vishnu <saivishnu725@gmail.com>
2021-11-28 17:58:46 +0530

    another pattern that kinda works

---
+++ practice/6.7b-187.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, 
+	return 0;
+}
+/*
+ 
+
+int main()
+{
+	int i, j, n;
+	printf("Enter the number of lines : ");
+	scanf("%d", &n);
+	for(i = n; i >= 1; i--)
+	{
+		for(j = 1; j <= i; j++)
+			printf("* ");
+		printf("\n");
+	}
+	return 0;
+}
+o/p
+Enter the number of lines : 3
+* * *
+* *
+*
+*/
+
