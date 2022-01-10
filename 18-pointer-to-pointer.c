63f78e0ed94356f0459aa1f734cf574f6b9db75b
Sai Vishnu <saivishnu725@gmail.com>
2022-01-10 14:51:05 +0530

    Some pointer and math.h stuffs

---
+++ 18-pointer-to-pointer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+		int a, *b,**c,***d,****e;
+		a = 100;
+		b = &a;
+		c = &b;
+		d= &c;
+		e= &d;
+		printf("a = %d\tb=%d\tc=%d\td=%d\te=%d\n",a,b,c,d,e);
+		printf("%d\t%d\t%d\n",a,a+*b,**c-***d+****e);	
+		return 0;
+}
---
+++ 19-math-f.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+		//ceil()
+		printf("Ceil = %f\n", ceil(4.6));
+		// floor()
+		printf("Floor = %f\n", floor(4.6));
+		//fabs()
+		printf("Fabs = %f\n", fabs(-2));
+		//sqrt()
+		printf("sqrt = %f\n", sqrt(2));
+		//pow()
+		printf("Power = %f\n", pow(2,3));
+		//log()
+		printf("Log = %f\n", log(4.0));
+		// log10() 
+		printf("Log10 = %f\n", log10(100.0));
+		// exp()
+		printf("Exp = %f\n", exp(4.0));
+		// cos()
+		printf("Cosine = %f\n", cos(0.523599));
+		return 0;
+}
