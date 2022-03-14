a7fb6e7af3efe3cdd82f5e853900420c059a2abe
Sai Vishnu <saivishnu725@gmail.com>
2022-03-14 21:23:59 +0530

    Stuffs

--- 36-isprime.c
+++ 36-isprime.c
@@ -1,3 +1,26 @@
 #include<stdio.h>
 
-int isprime
+void isprime(int n)
+{
+		int c = 0, i;
+		
+		for(i = 2; i < n; i++)
+				if( n % i == 0)
+						c++;
+		
+		if(c == 0)
+				printf("%d is a prime number.\n", n);
+		else
+				printf("%d is not a prime number.\n", n);
+}
+
+int main()
+{
+		int a;
+		
+		printf("Enter the number: ");
+		scanf("%d", &a);
+		
+		isprime(a);
+		return 0;
+}
---
+++ 38-trace_matrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+
+		int a[10][10], n, i, j, sum = 0;
+
+		printf ("Enter order of the square matrix: ");
+		scanf ("%d", &n);
+		
+		printf ("Enter the matrix\n");
+		for(i = 0;i < n; i++)
+				for(j = 0;j < n; j++)
+						scanf("%d", &a[i][j]);
+
+		printf ("The entered matrix: \n");
+		for(i = 0;i < n; i++)
+		{
+				for(j = 0;j < n; j++)
+						printf("%d ", a[i][j]);
+				printf("\n"); 
+		}
+
+		for(i = 0;i < n; i++)
+		    sum += a[i][i];
+		printf ("Trace of the matrix = %d\n", sum);
+}
+
